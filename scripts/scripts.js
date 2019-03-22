01c5524b744d176a0d9f38d8f50c1054474558eb
Ivan <ivan.anikukhin@gmail.com>
2019-03-22 13:43:50 +0300

    Add style to js-tests section

---
+++ scripts/scripts.js
@@ -0,0 +1,9 @@
+﻿/* first test */
+var arr = document.getElementsByClassName("tests-title");
+arr[1].innerHTML = "Первый тест, выбрал элемент по class и изменил его";
+/* second test */
+var elem = document.getElementById("tests-title");
+elem.innerHTML = "Второй тест, выбрал элемент по id и изменил его";
+/* third test */
+var arr = document.getElementsByTagName("h2");
+arr[6].innerHTML = "Третий тест, выбрал элемент по tag name и изменил его";
